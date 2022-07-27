47be5fe762afda089526c2ba7b3d505868ba4276
“Kempt101” <Kempt101@gmail.com>
2022-07-26 23:34:13 -0500

    Add function to create book cards using object's properties from array

--- app.js
+++ app.js
@@ -16,7 +16,14 @@ window.addEventListener("click", (event) => {
   }
 });
 
-let library = [];
+let library = [
+  {
+    title: "Harry Potter The Chamber of Secrets",
+    author: "J.K. Rowling",
+    pages: 251,
+    read: "yes",
+  },
+];
 
 class Book {
   constructor(title, author, pages, read) {
@@ -29,15 +36,46 @@ class Book {
   showInfo() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
   }
+
+  //function that creates a card on the page based off an objects properties within the array
+  createBookCard() {
+    const mainContainer = document.getElementById("main-container");
+    const bookCard = document.createElement("div");
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const read = document.createElement("p");
+    mainContainer.appendChild(bookCard);
+    bookCard.append(title, author, pages, read);
+    bookCard.classList.add("book-card");
+    title.textContent = this.title;
+    author.textContent = this.author;
+    pages.textContent = this.pages;
+    read.textContent = this.read;
+  }
 }
 
-const theHobbit = new Book(
-  "The Hobbit",
-  "J.R.R. Tolkien",
-  295,
-  "has been read"
-);
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "Yes");
 
-// Takes user input and store the new book objects into an array. use document.getelementbyid and store values into variables within the function
+let addBookBtn = document.getElementById("book-submit");
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+let pages = document.getElementById("pages");
+let read = document.getElementById("read");
 
-function addBookToLibrary(book) {}
+// Takes user input and store the new book objects into an array
+function addBookToLibrary() {
+  return library.push(
+    new Book(title.value, author.value, Number(pages.value), read.value)
+  );
+}
+addBookBtn.addEventListener("click", function () {
+  addBookToLibrary();
+  modal.style.display = "none";
+  addBookBtn.form.reset();
+});
+
+// Write a function that loops through the array and displays each book on the page.
+function addBookCard() {
+  for (let i = 0; i < library.length; i++) {}
+}
