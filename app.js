58542525cf26cafcdfae95898b2625377abd90cb
Sean Kempt <kempt101@gmail.com>
2022-07-27 22:08:13 -0500

    Update format of logic

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ let modal = document.getElementById("bookmodal");
 let bookbtn = document.getElementById("new-book");
 let close = document.getElementsByClassName("close")[0];
 
+let addBookBtn = document.getElementById("book-submit");
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+let pages = document.getElementById("pages");
+let read = document.getElementById("read");
+
 bookbtn.addEventListener("click", () => {
   modal.style.display = "block";
 });
@@ -25,6 +31,49 @@ let library = [
   },
 ];
 
+class App {
+  constructor() {
+    addBookBtn.addEventListener("click", this._newBook.bind(this));
+  }
+
+  // Takes user input and store the new book objects into an array
+  addBookToLibrary() {
+    library.push(
+      new Book(title.value, author.value, Number(pages.value), read.value)
+    );
+  }
+
+  _newBook() {
+    this.addBookToLibrary();
+    modal.style.display = "none";
+    addBookBtn.form.reset();
+  }
+
+  // Write a function that loops through the array and displays each book on the page.
+  addBookCard() {
+    for (let i = 0; i < library.length; i++) {
+      createBookCard(library[i]);
+    }
+  }
+
+  //function that creates a card on the page based off an objects properties within the array
+  createBookCard(book) {
+    const mainContainer = document.getElementById("main-container");
+    const bookCard = document.createElement("div");
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const read = document.createElement("p");
+    mainContainer.appendChild(bookCard);
+    bookCard.append(title, author, pages, read);
+    bookCard.classList.add("book-card");
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    read.textContent = book.read;
+  }
+}
+
 class Book {
   constructor(title, author, pages, read) {
     this.title = title;
@@ -32,52 +81,14 @@ class Book {
     this.pages = pages;
     this.read = read;
   }
-
-  showInfo() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
-  }
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "Yes");
+const app = new App();
 
-let addBookBtn = document.getElementById("book-submit");
-let title = document.getElementById("title");
-let author = document.getElementById("author");
-let pages = document.getElementById("pages");
-let read = document.getElementById("read");
+//example for adding event handler/listener to classes in OOP for JavaScript
+//You have to bind This because this within an event handler is going to point to the HTML element and not the object.
+//Event listener Should be declared within the constructor function of a class because the constructor area/scope/context is what loads when a new object is created.
+// form.addEventListener("submit", this.function.bind(this));
 
-// Takes user input and store the new book objects into an array
-function addBookToLibrary() {
-  return library.push(
-    new Book(title.value, author.value, Number(pages.value), read.value)
-  );
-}
-addBookBtn.addEventListener("click", function () {
-  addBookToLibrary();
-  modal.style.display = "none";
-  addBookBtn.form.reset();
-});
-
-// Write a function that loops through the array and displays each book on the page.
-function addBookCard() {
-  for (let i = 0; i < library.length; i++) {
-    createBookCard(library[i]);
-  }
-}
-
-//function that creates a card on the page based off an objects properties within the array
-function createBookCard(book) {
-  const mainContainer = document.getElementById("main-container");
-  const bookCard = document.createElement("div");
-  const title = document.createElement("p");
-  const author = document.createElement("p");
-  const pages = document.createElement("p");
-  const read = document.createElement("p");
-  mainContainer.appendChild(bookCard);
-  bookCard.append(title, author, pages, read);
-  bookCard.classList.add("book-card");
-  title.textContent = book.title;
-  author.textContent = book.author;
-  pages.textContent = book.pages;
-  read.textContent = book.read;
-}
+//notes
+//might need to move the createbookcard and the add book card to the Book Class
